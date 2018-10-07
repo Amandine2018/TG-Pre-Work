7365f19efdb29f4336f47d1f38d2ba1b7572c6a2
Amandine Velamala <>
2018-10-07 17:11:51 -0600

    ternary operators

--- ex022JSConditional.js
+++ ex022JSConditional.js
@@ -1,6 +1,11 @@
-let tool = 'marker';
+let isLocked = false;
 
-// Use short circuit evaluation to assign  writingUtensil variable below:
-let writingUtensil = tool || 'pen';
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
 
-console.log(`The ${writingUtensil} is mightier than the sword.`);
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
