03fac575f504cc2470972e40894588841e48b15b
Amandine Velamala <>
2018-10-07 16:35:54 -0600

    try different challenges with variables

--- ex021JSVariables.js
+++ ex021JSVariables.js
@@ -1,4 +1,16 @@
-let newVariable = 'Playing around with typeof.';
-console.log(typeof newVariable);
-newVariable = 1;
-console.log(typeof newVariable);
+let myString = 'test concatenation with ';
+let myBoolean = true;
+let test = myString + myBoolean;
+console.log(test);
+console.log(typeof test);
+
+let myPlace1 = 'Italy';
+let myPlace2 = 'Spain';
+console.log(`I want to travel to ${myPlace1} and ${myPlace2} .`);
+
+
+let test1 = 'figuring out quirks';
+let num = 8;
+test1 += num;
+console.log(test1);
+console.log(typeof test1);
