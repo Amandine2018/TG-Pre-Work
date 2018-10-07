f763f14a7773eb228bc26fd102a29c5cd25d5078
Amandine Velamala <>
2018-10-07 17:19:26 -0600

    switch

--- ex022JSConditional.js
+++ ex022JSConditional.js
@@ -1,14 +1,16 @@
-let season = 'summer';
+let athleteFinalPosition = 'first place';
 
-if (season === 'spring'){
-  console.log('It\'s spring! The trees are budding!');
-} else if (season === 'winter') {
-  console.log('It\'s winter! Everything is covered in snow.');
-}  else if (season === 'fall') {
-  console.log('It\'s fall! Leaves are falling!');
-} else if (season === 'summer') {
-  console.log('It\'s sunny and warm because it\'s summer!');
-}
- else {
-  console.log('Invalid season.');
-}
+ switch (athleteFinalPosition) {
+  case 'first place':
+   	console.log('You get the gold medal!');
+    break;
+  case 'second place':
+     console.log('You get the silver medal!');
+     break;
+   case 'third place':
+     console.log('You get the bronze medal!');
+     break;
+   default:
+     console.log('No medal awarded.');
+     break;
+ }
