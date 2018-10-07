d27963bfb9e625d07f9bba1d28903aa9b1116ebd
Amandine Velamala <>
2018-10-07 16:24:39 -0600

    typeof operator returns a string of the data type

--- ex021JSVariables.js
+++ ex021JSVariables.js
@@ -1,3 +1,4 @@
-let myName = 'Amandine';
-let myCity = 'Paris';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
