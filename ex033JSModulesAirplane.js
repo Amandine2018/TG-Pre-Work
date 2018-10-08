492a621976b485de2c950dc1a1860db33b466440
Amandine Velamala <>
2018-10-08 10:36:56 -0600

    export as (in the airplane folder)

--- ex033JSModulesAirplane.js
+++ ex033JSModulesAirplane.js
@@ -1,19 +1,24 @@
+export { availableAirplanes as aircrafts, flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs };
+
 let availableAirplanes = [
-{
-  name: 'AeroJet',
-  fuelCapacity: 800,
-  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators']
- },
- {
-  name: 'SkyJet',
-  fuelCapacity: 500,
-  availableStaff: ['pilots', 'flightAttendants']
- }
+{name: 'AeroJet',
+ fuelCapacity: 800,
+ availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ maxSpeed: 1200,
+ minSpeed: 300
+},
+{name: 'SkyJet',
+ fuelCapacity: 500,
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
+}
 ];
 
- let flightRequirements = {
-   requiredStaff: 4
- }
+let flightRequirements = {
+  requiredStaff: 4,
+  requiredSpeedRange: 700
+};
 
 function meetsStaffRequirements(availableStaff, requiredStaff) {
   if (availableStaff.length >= requiredStaff) {
@@ -23,4 +28,11 @@ function meetsStaffRequirements(availableStaff, requiredStaff) {
   }
 };
 
-export { availableAirplanes, flightRequirements, meetsStaffRequirements };
+function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+  let range = maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
+    return true;
+    } else {
+    return false;
+  }
+};
