bf10872932a9167fd7a7bf7631b11ea1ec8aa4a8
Amandine Velamala <>
2018-10-08 08:57:58 -0600

    create a parent class

--- ex031JSClasses.js
+++ ex031JSClasses.js
@@ -1,32 +1,19 @@
-class Surgeon {
-  constructor(name, department) {
+class HospitalEmployee {
+  constructor(name){
     this._name = name;
-    this._department = department;
     this._remainingVacationDays = 20;
   }
 
-	get name() {
-  	return this._name;
-	}
-
-	get department() {
-  	return this._department;
-	}
+  get name() {
+    return this._name;
+  }
 
   get remainingVacationDays() {
     return this._remainingVacationDays;
   }
 
   takeVacationDays(daysOff) {
-    this._remainingVacationDays -= daysOff;
+    return this._remainingVacationDays -= daysOff;
   }
+
 }
-
-const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
-const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
-
-console.log(surgeonCurry.name);
-
-surgeonCurry.takeVacationDays(3);
-
-console.log(surgeonCurry.remainingVacationDays);
