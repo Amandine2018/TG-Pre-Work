b707f18fafdae9a5b0858b548d3c76dc22aa43c5
Amandine Velamala <>
2018-10-08 10:28:44 -0600

    named imports

--- ex034JSModulesMission.js
+++ ex034JSModulesMission.js
@@ -1,28 +1,15 @@
-import { availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
-
-import meetsSpeedRangeRequirements from './airplane';
+import { availableAirplanes, flightRequirements, meetsStaffRequirements } from './airplane';
 
 function displayFuelCapacity() {
-  availableAirplanes.forEach(function(element){
-    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
-  });
-}
-
-displayFuelCapacity();
-
-function displayStaffStatus(){
-  availableAirplanes.forEach(function(element){
-    console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff));
-  }
-)};
-
-function displaySpeedRangeStatus() {
   availableAirplanes.forEach(function(element) {
-    console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+    console.log('Fuel Capacity of ' + element['name'] + ': ' + element['fuelCapacity']);
   });
 }
 
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element){
+    console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff,flightRequirements.requiredStaff));
+  });
+}
 
 displayStaffStatus();
-
-displaySpeedRangeStatus();
