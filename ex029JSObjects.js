9ca788980a1c6d1ffde30a4e3fd954ffc95a28e4
Amandine Velamala <>
2018-10-07 22:26:26 -0600

    objects are passed by reference

--- ex029JSObjects.js
+++ ex029JSObjects.js
@@ -1,32 +1,18 @@
 let spaceship = {
-  passengers: [{ name: 'Tom'}],
-  telescope: {
-    yearBuilt: 2018,
-    model: "91031-XLT",
-    focalLength: 2032
-  },
-  crew: {
-    captain: {
-      name: 'Sandra',
-      degree: 'Computer Engineering',
-      encourageTeam() { console.log('We got this!') },
-     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
-  },
-  engine: {
-    model: "Nimbus2000"
-  },
-  nanoelectronics: {
-    computer: {
-      terabytes: 100,
-      monitors: "HD"
-    },
-    backup: {
-      battery: "Lithium",
-      terabytes: 50
-    }
-  }
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
 };
 
-const capFave = spaceship.crew.captain['favorite foods'][0];
+// Write your code below
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+};
 
-const firstPassenger = spaceship.passengers[0];
+let remotelyDisable = obj => {
+	obj.disabled = true
+};
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+
+console.log(spaceship);
