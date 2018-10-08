c65d8a5a1099b4d53df7ed79f2fd4c6a9698ba85
Amandine Velamala <>
2018-10-08 08:38:02 -0600

    create a class instance

--- ex031JSClasses.js
+++ ex031JSClasses.js
@@ -1,6 +1,10 @@
 class Surgeon {
-	constructor(name, department) {
+  constructor(name, department) {
     this.name = name;
     this.department = department;
   }
 }
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
