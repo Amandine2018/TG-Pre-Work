f8280bd31cede756045cdd5f7e80aa51548765b9
Amandine Velamala <>
2018-10-08 09:49:45 -0600

    export using export default syntax

--- ex033JSModulesAirplane.js
+++ ex033JSModulesAirplane.js
@@ -1,8 +1,40 @@
-const Airplane = {};
+export let availableAirplanes  = [
+  {
+  	name: 'AeroJet',
+  	fuelCapacity: 800,
+    availableStaff:['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+    maxSpeed: 1200,
+    minSpeed: 300
+  },
+  {
+  	name: 'SkyJet',
+  	fuelCapacity: 500,
+    availableStaff:['pilots', 'flightAttendants'],
+    maxSpeed: 800,
+    minSpeed: 200
+  }
+];
 
-module.exports = {
-  myAirplane: "CloudJet",
-  displayAirplane: function() {
-  	return this.myAirplane;
-	}
+export let flightRequirements = {
+  requiredStaff: 4,
+  requiredSpeedRange: 700
 };
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange){
+  let range= maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export default meetsSpeedRangeRequirements;
