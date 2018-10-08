fc8dc80f2729a6c5c2feedb3ebfebadf4e1d831e
Amandine Velamala <>
2018-10-07 21:14:42 -0600

    functions as parameters

--- ex027JSHigherOrderFunctions.js
+++ ex027JSHigherOrderFunctions.js
@@ -1,13 +1,31 @@
-let checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
   for(let i = 1; i <= 1000000; i++) {
     if ( (2 + 2) != 4) {
       console.log('Something has gone very wrong :( ');
     }
   }
-}
+};
 
-const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+const addTwo = num => num + 2;
 
-is2p2();
+const timeFuncRuntime = funcParameter => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
 
-console.log(is2p2.name);
+// Write your code below
+const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+
+const checkConsistentOutput = (fun , val) => {
+  let first = fun(val);
+  let second = fun(val);
+    if (first === second){
+    	return first;
+  	} else {
+     return 'This function returned inconsistent results';
+    }
+};
+
+checkConsistentOutput(addTwo, 20);
