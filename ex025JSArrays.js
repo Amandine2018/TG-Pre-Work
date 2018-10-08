d42e76d1d96e89545c7fce4828d3e4dd3de6e6c4
Amandine Velamala <>
2018-10-07 19:01:53 -0600

    update array elements

--- ex025JSArrays.js
+++ ex025JSArrays.js
@@ -1,9 +1,5 @@
-const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+let groceryList = ['bread', 'tomatoes', 'milk'];
 
-const listItem = famousSayings[0];
+groceryList[1] = 'avocados';
 
-console.log(listItem);
-
-console.log(famousSayings[2]);
-
-console.log(famousSayings[3]);
+console.log(groceryList);
