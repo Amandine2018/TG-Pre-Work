48a941214763672d85193035de1a3131cf1d6c03
Amandine Velamala <>
2018-10-07 21:38:12 -0600

    .filter() method

--- ex028JSIterators.js
+++ ex028JSIterators.js
@@ -1,18 +1,17 @@
-const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
 
-// Create the secretMessage array below
-const secretMessage = animals.map(animal => {
-  return animal[0];
-});
-
-console.log(secretMessage.join(''));
-
-const bigNumbers = [100, 200, 300, 400, 500];
-
-// Create the smallNumbers array below
-const smallNumbers = bigNumbers.map(num => {
-	return num / 100;
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(num => {
+  return num < 250;
 });
 
 console.log(smallNumbers);
-                                   
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7;
+});
+
+console.log(longFavoriteWords);
