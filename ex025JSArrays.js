4d79b5a44263ed4657f9003af6479da48078643c
Amandine Velamala <>
2018-10-07 19:00:34 -0600

    accessing array elements using index: arrayName[index#]

--- ex025JSArrays.js
+++ ex025JSArrays.js
@@ -1,2 +1,9 @@
-const hobbies = ['camping', 'book club', 'dance class'];
-console.log(hobbies);
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+
+console.log(listItem);
+
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
