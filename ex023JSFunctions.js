cd2a275cdf8d2d9daa0eda1e5c2063e6262fe96c
Amandine Velamala <>
2018-10-07 18:14:13 -0600

    refactor function to be a concise body, call function to check

--- ex023JSFunctions.js
+++ ex023JSFunctions.js
@@ -1,10 +1,4 @@
-const plantNeedsWater = (day) => {
-  if (day === 'Wednesday') {
-    return true;
-  } else {
-    return false;
-  }
-}
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
 
 console.log(plantNeedsWater('Tuesday'));
 console.log(plantNeedsWater('Wednesday'));
