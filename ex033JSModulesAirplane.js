a6dc48b1cfbe45347295f072f5d33c740b922765
Amandine Velamala <>
2018-10-08 10:16:00 -0600

    named exports

--- ex033JSModulesAirplane.js
+++ ex033JSModulesAirplane.js
@@ -1,24 +1,19 @@
-export let availableAirplanes  = [
-  {
-  	name: 'AeroJet',
-  	fuelCapacity: 800,
-    availableStaff:['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
-    maxSpeed: 1200,
-    minSpeed: 300
-  },
-  {
-  	name: 'SkyJet',
-  	fuelCapacity: 500,
-    availableStaff:['pilots', 'flightAttendants'],
-    maxSpeed: 800,
-    minSpeed: 200
-  }
+let availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800,
+  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators']
+ },
+ {
+  name: 'SkyJet',
+  fuelCapacity: 500,
+  availableStaff: ['pilots', 'flightAttendants']
+ }
 ];
 
-export let flightRequirements = {
-  requiredStaff: 4,
-  requiredSpeedRange: 700
-};
+ let flightRequirements = {
+   requiredStaff: 4
+ }
 
 function meetsStaffRequirements(availableStaff, requiredStaff) {
   if (availableStaff.length >= requiredStaff) {
@@ -28,13 +23,4 @@ function meetsStaffRequirements(availableStaff, requiredStaff) {
   }
 };
 
-function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange){
-  let range= maxSpeed - minSpeed;
-  if (range > requiredSpeedRange) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export default meetsSpeedRangeRequirements;
+export { availableAirplanes, flightRequirements, meetsStaffRequirements };
